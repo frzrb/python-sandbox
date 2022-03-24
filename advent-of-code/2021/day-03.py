38a82cd40ebc0e081a8df7cbee6129c9fe76995f
frzrb <freezerburn@gmail.com>
2022-03-23 20:55:49 -0400

    Adding day 3 data and wip

---
+++ advent-of-code/2021/day-03.py
@@ -0,0 +1,20 @@
+input_file = "advent-of-code/2021/day-03-input.txt"
+w, h = 12, 5
+count = 0
+data_array = [[0 for x in range(w)] for y in range(h)] 
+
+test_line = "110110100001"
+
+# data_file: open input_file as read-only
+#with open(input_file, 'r') as data_file:
+#    # 
+#    total_lines = sum(1 for line in data_file)
+#    for line in data_file:
+#        for z in range(w):
+#            data_array[count][z] = 
+
+
+
+
+#data_array[0][0] = 1
+
